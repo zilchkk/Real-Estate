09723c2a7204ee2acf9bbbb73a000018d83c0260
zilch <129466204+zilchkk@users.noreply.github.com>
2024-08-17 12:03:49 +0530

    Add files via upload

---
+++ src/components/Get started/Getstarted.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Getstarted.css"
+export const Getstarted = () => {
+  return (
+    <div className="g-wrapper">
+      <div className="g-center">
+        <div className="MainText">Get Started with Home Tour</div>
+        <span className="secondText">Subscribe and find super cute House from us.</span>
+        <span className="secondText">Find Your residence soon</span>
+        <button className="g-button">Get started</button>
+      </div>
+    </div>
+  );
+};
